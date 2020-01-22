926bac508767378331975915c2cce0a2b5841ca7
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-01-22 15:11:39 +0100

    Login form, validation finished

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,8 +2,57 @@ import React, {Component} from 'react';
 import HeaderWelcome from "./../Welcome/HeaderWelcome";
 import WelcomeFooter from "./../Welcome/WelcomeFooter";
 import {NavLink} from "react-router-dom";
+import errorIcon from './../../assets/error.png'
+import ReactTooltip from 'react-tooltip'
 
 class Login extends Component {
+    state = {
+        email: "",
+        password: "",
+        errorEmail: false,
+        errorPassword: false,
+    };
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    };
+
+    handleFormSubmit = (e) => {
+
+        this.setState({
+            errorEmail: false,
+            errorPassword: false,
+        });
+
+        e.preventDefault();
+
+        const {password, email} = this.state;
+
+        const emailValidation = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (emailValidation.test(email) &&
+            password.length >= 6) {
+
+            this.setState({
+                email: "",
+                password: "",
+            })
+
+        } else {
+            if (password.length < 6) {
+                this.setState({
+                    errorPassword: true,
+                })
+            }
+            if (!emailValidation.test(email)) {
+                this.setState({
+                    errorEmail: true,
+                })
+            }
+        }
+    };
 
     render() {
 
@@ -12,12 +61,30 @@ class Login extends Component {
                 <HeaderWelcome/>
 
                 <section className='login'>
-                    <form className='login__form'>
-                        <label for='email'>Email</label>
-                        <input type='text' name='email' id='email'/>
+                    <form className='login__form' onSubmit={this.handleFormSubmit}>
+                        <label htmlFor='email'>{this.state.errorEmail &&
+                        <>
+                            <span data-for='error__email' data-tip="">
+                                <img className='error__icon' src={errorIcon} alt='error'/>
+                            </span>
+                            <ReactTooltip className='error__class' id='error__email' type='error' delayHide={2000} effect='solid'>
+                                <span>Wrong email</span>
+                            </ReactTooltip>
+                        </>
+                        } Email</label>
+                        <input type='text' name='email' id='email' value={this.state.email} onChange={this.handleChange}/>
 
-                        <label for='password'>Password</label>
-                        <input type='password' name='password' id='password'/>
+                        <label htmlFor='password'>{this.state.errorPassword &&
+                        <>
+                            <span data-for='error__password' data-tip="">
+                                <img className='error__icon' src={errorIcon} alt='error'/>
+                            </span>
+                            <ReactTooltip className='error__class' id='error__password' type='error' delayHide={2000} effect='solid'>
+                                <span>Wrong password</span>
+                            </ReactTooltip>
+                        </>
+                        } Password</label>
+                        <input type='password' name='password' id='password' value={this.state.password} onChange={this.handleChange}/>
 
                         <div className='login__form__buttons'>
 
@@ -25,7 +92,8 @@ class Login extends Component {
                                 <button className='buttons__small' type="submit" value="Submit">Register</button>
                             </NavLink>
 
-                            <button className='buttons__small' type="submit" value="Submit">Login</button>
+                            <button className='buttons__small' type="submit" value="Submit"
+                                    onClick={this.handleFormSubmit}>Login</button>
 
                         </div>
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,8 +2,79 @@ import React, {Component} from 'react';
 import HeaderWelcome from "./../Welcome/HeaderWelcome";
 import WelcomeFooter from "./../Welcome/WelcomeFooter";
 import {NavLink} from "react-router-dom";
+import errorIcon from './../../assets/error.png'
+import ReactTooltip from 'react-tooltip';
 
 class Register extends Component {
+    state = {
+        login: "",
+        email: "",
+        password: "",
+        passwordConfirm: "",
+        errorEmail: false,
+        errorPassword: false,
+        errorPassword2: false,
+        errorLogin: false,
+        registerError: false,
+    };
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    };
+
+    handleFormSubmit = (e) => {
+
+        this.setState({
+            errorPassword: false,
+            errorPassword2: false,
+            errorEmail: false,
+            errorLogin: false,
+        });
+
+        e.preventDefault();
+
+        const {password, passwordConfirm, email, login} = this.state;
+
+        const emailValidation = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if ( emailValidation.test(email) &&
+            password.length >= 6 &&
+            passwordConfirm.length >= 6 &&
+            passwordConfirm === password &&
+            login.length >= 6) {
+
+            this.setState({
+                login: "",
+                email: "",
+                password: "",
+                passwordConfirm: "",
+            })
+
+        } else {
+            if (password.length < 6) {
+                this.setState({
+                    errorPassword: true,
+                })
+            }
+            if (passwordConfirm.length < 6 || passwordConfirm !== password) {
+                this.setState({
+                    errorPassword2: true,
+                })
+            }
+            if (!emailValidation.test(email)) {
+                this.setState({
+                    errorEmail: true,
+                })
+            }
+            if (login.length < 6) {
+                this.setState({
+                    errorLogin: true,
+                })
+            }
+        }
+    };
 
     render() {
 
@@ -12,27 +83,67 @@ class Register extends Component {
                 <HeaderWelcome/>
 
                 <section className='register'>
-                    <form className='register__form'>
-                        <label for='login'>Login</label>
-                        <input type='text' name='login' id='login'/>
 
-                        <label for='email'>Email</label>
-                        <input type='text' name='email' id='email'/>
+                    <form className='register__form' onSubmit={this.handleFormSubmit}>
 
-                        <label for='password'>Password</label>
-                        <input type='password' name='password' id='password'/>
+                        <label htmlFor='login'>{this.state.errorLogin &&
+                        <>
+                            <span data-for='error__login' data-tip="">
+                                <img className='error__icon' src={errorIcon} alt='error'/>
+                            </span>
+                            <ReactTooltip className='error__class' id='error__login' type='error' delayHide={2000} effect='solid'>
+                                <span>Login should have more <br/> than 6 characters.</span>
+                            </ReactTooltip>
+                        </>
+                        } Login</label>
 
-                        <label for='password_confirm'>Password confirmation</label>
-                        <input type='password' name='password_confirm' id='password_confirm'/>
+                        <input type='text' name='login' id='login'
+                               value={this.state.login} onChange={this.handleChange}/>
+
+                        <label htmlFor='email'>{this.state.errorEmail &&
+                        <>
+                            <span data-for='error__email' data-tip="">
+                                <img className='error__icon' src={errorIcon} alt='error'/>
+                            </span>
+                            <ReactTooltip className='error__class' id='error__email' type='error' delayHide={2000} effect='solid'>
+                                <span>Email should have @.</span>
+                            </ReactTooltip>
+                        </>
+                        } Email</label>
+                        <input type='text' name='email' id='email'
+                               value={this.state.email} onChange={this.handleChange}/>
+
+                        <label htmlFor='password'>{this.state.errorPassword &&
+                        <>
+                            <span data-for='error__password' data-tip="">
+                                <img className='error__icon' src={errorIcon} alt='error'/>
+                            </span>
+                            <ReactTooltip className='error__class' id='error__password' type='error' delayHide={2000} effect='solid'>
+                                <span>Password should have<br/> more than 6 characters.</span>
+                            </ReactTooltip>
+                        </>
+                        } Password</label>
+                        <input type='password' name='password' id='password'
+                               value={this.state.password} onChange={this.handleChange}/>
+
+                        <label htmlFor='passwordConfirm'>{this.state.errorPassword2 &&
+                        <>
+                            <span data-for='error__password2' data-tip="">
+                                <img className='error__icon' src={errorIcon} alt='error'/>
+                            </span>
+                            <ReactTooltip className='error__class' id='error__password2' type='error' delayHide={2000} effect='solid'>
+                                <span>Passwords are not the same.</span>
+                            </ReactTooltip>
+                        </>
+                        } Password confirmation</label>
+                        <input type='password' name='passwordConfirm' id='password__confirm'
+                               value={this.state.passwordConfirm} onChange={this.handleChange}/>
 
                         <div className='register__form__buttons'>
-
                             <NavLink to='/login'>
-                                <button className='buttons__small' type="submit" value="Submit">Login</button>
+                                <button className='buttons__small'>Login</button>
                             </NavLink>
-
-                            <button className='buttons__small' type="submit" value="Submit">Register</button>
-
+                            <button className='buttons__small' onClick={this.handleFormSubmit}>Register</button>
                         </div>
 
                     </form>
