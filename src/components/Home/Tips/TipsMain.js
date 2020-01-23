5c24ac3e445d0e1efa30aa9656811ee03ce7936d
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-01-23 17:58:12 +0100

    Tips section, added newest and popular tips, carousel

--- src/components/Home/Tips/TipsMain.js
+++ src/components/Home/Tips/TipsMain.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 import tipPhoto from './../../../assets/weekly-tip.jpg'
 import firebase from "../../firebase/firebase";
+import errorIcon from "../../../assets/error.png";
+import ReactTooltip from "react-tooltip";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import tip1 from './../../../assets/article1.jpg'
+import tip2 from './../../../assets/article2.jpg'
+import tip3 from './../../../assets/article3.jpg'
+import tip4 from './../../../assets/article4.jpg'
+import tip5 from './../../../assets/article5.jpg'
+import tip6 from './../../../assets/article6.jpg'
 
 class TipsMain extends Component {
     state = {
         question: "",
+        questionError: false,
     };
 
     handleChange = (e) => {
@@ -19,10 +30,19 @@ class TipsMain extends Component {
         e.preventDefault();
         const { question } = this.state;
 
-        console.log(user, question);
-        this.setState({
-            question: "",
-        })
+        if (question.length > 100) {
+            this.setState({
+                question: "",
+                questionError: false,
+            });
+            console.log(user, question);
+        } else {
+            this.setState({
+                questionError: true,
+            })
+        }
+
+
     };
 
     render() {
@@ -51,19 +71,19 @@ class TipsMain extends Component {
                                 quis interdum nunc rutrum.
                             </p>
                             <div className='tips__week__description__button'>
-                                <button className='buttons__small'>Zalicz trening</button>
+                                <button className='buttons__small'>Take the test</button>
                             </div>
                             <ul className='tips__week__description__tags'>
                                 <li>
                                     <i className="fas fa-tags"></i>
                                 </li>
-                                <li>bezpieczeństwo</li>
+                                <li>safety</li>
                                 <li>|</li>
-                                <li>hamowanie</li>
+                                <li>braking</li>
                                 <li>|</li>
-                                <li>technika</li>
+                                <li>technique</li>
                                 <li>|</li>
-                                <li>żywotność</li>
+                                <li>engine</li>
                                 <li>|</li>
                             </ul>
                             <div className='tips__week__description__sharing'>
@@ -83,10 +103,50 @@ class TipsMain extends Component {
                         <textarea value={this.state.question} onChange={this.handleChange}></textarea>
                     </form>
                     <div className='tips__button'>
-                        <span onClick={this.handleSubmit} className='buttons__small'>Send</span>
+                        {this.state.questionError &&
+                        <>
+                            <span data-for='error__email' data-tip="">
+                                <img className='error__icon' src={errorIcon} alt='error'/>
+                            </span>
+                            <ReactTooltip className='error__class' id='error__email' type='error' delayHide={2000} effect='solid'>
+                                <span>The question should have at least 100 characters!</span>
+                            </ReactTooltip>
+                        </>}
+                        <span onClick={this.handleSubmit} className='buttons__small'>Send </span>
                     </div>
-                    <div className='tips__newest'>
-
+                    <div className='tips__newest__popular'>
+                        <h2>Newest Tips</h2>
+                        <Carousel showThumbs={false} infiniteLoop showStatus={false}>
+                            <div>
+                                <h3>Car and Driver [23]</h3>
+                                <img alt="tip1" src={tip1} />
+                            </div>
+                            <div>
+                                <h3>Mobile phones and driving safety [24]</h3>
+                                <img alt="tip2" src={tip2} />
+                            </div>
+                            <div>
+                                <h3>Speed limits [25]</h3>
+                                <img alt="tip3" src={tip3} />
+                            </div>
+                        </Carousel>
+                    </div>
+                    <div className='tips__newest__popular'>
+                        <h2>Popular Tips</h2>
+                        <Carousel showThumbs={false} infiniteLoop showStatus={false}>
+                            <div>
+                                <h3>Safety tips for truck drivers [20]</h3>
+                                <img alt="tip4" src={tip4} />
+                            </div>
+                            <div>
+                                <h3>Aggressive driving and road rage [04]</h3>
+                                <img alt="tip5" src={tip5} />
+                            </div>
+                            <div>
+                                <h3>Winter driving preparedness [11]</h3>
+                                <img alt="tip6" src={tip6} />
+                            </div>
+                        </Carousel>
                     </div>
                 </section>
             </>
